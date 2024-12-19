abd64f7802c2d69ad030df95739738a2df79977c
Yohan678 <beginnerdevyohan@gmail.com>
2024-12-19 17:13:49 -0500

    Initial Commit

---
+++ Daily Mission/Daily_MissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_MissionApp.swift
+//  Daily Mission
+//
+//  Created by Yohan Yoon on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Daily_MissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Daily MissionTests/Daily_MissionTests.swift
@@ -0,0 +1,17 @@
+//
+//  Daily_MissionTests.swift
+//  Daily MissionTests
+//
+//  Created by Yohan Yoon on 12/19/24.
+//
+
+import Testing
+@testable import Daily_Mission
+
+struct Daily_MissionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
